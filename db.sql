031950953273614778d85e846ce19dbd3ddb2909
JAFB-201801287 <75402151+JAFB-201801287@users.noreply.github.com>
2020-12-21 10:26:00 -0600

    BASE DE DATOS

---
+++ db.sql
@@ -0,0 +1,89 @@
+CREATE DATABASE banca_virtual;
+USE banca_virtual;
+
+CREATE TABLE Cliente (
+	cui BIGINT PRIMARY KEY NOT NULL,
+    nit BIGINT NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    fecha_nacimiento VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Empresa (
+	id_empresa INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    nombre_comercial VARCHAR(100) NOT NULL,
+	nombre_representante VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE Usuario (
+	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+	contraseña VARCHAR(25) NOT NULL,
+    cui BIGINT,
+    id_empresa INT,
+    FOREIGN KEY (cui) REFERENCES Cliente(cui),
+    FOREIGN KEY (id_empresa) REFERENCES Empresa(id_empresa)
+);
+
+CREATE TABLE Cuenta (
+	id_cuenta INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    monto FLOAT(15,2) NOT NULL,
+    tipo_cuenta VARCHAR(50) NOT NULL,
+	tipo_moneda VARCHAR(50) NOT NULL,
+    id_usuario INT,
+    PRIMARY KEY (id_cuenta),
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
+);
+
+CREATE TABLE Transaccion (
+	id_transaccion INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    monto FLOAT(15,2) NOT NULL,
+    monto_anterior FLOAT(15,2) NOT NULL,
+    monto_despues FLOAT(15,2) NOT NULL,
+	tipo_moneda VARCHAR(50) NOT NULL,
+    tipo_transaccion VARCHAR(50) NOT NULL,
+    id_cuenta INT,
+    PRIMARY KEY (id_transaccion),
+    FOREIGN KEY (id_cuenta) REFERENCES Cuenta(id_cuenta)
+);
+
+CREATE TABLE Planilla (
+	id_planilla INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    monto FLOAT(15,2) NOT NULL,
+    monto_anterior FLOAT(15,2) NOT NULL,
+    monto_despues FLOAT(15,2) NOT NULL,
+    id_cuenta INT,
+    PRIMARY KEY (id_planilla),
+    FOREIGN KEY (id_cuenta) REFERENCES Cuenta(id_cuenta)
+);
+
+CREATE TABLE Prestamo (
+	id_prestamo INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    monto FLOAT(15,2) NOT NULL,
+    monto_anterior FLOAT(15,2) NOT NULL,
+    monto_despues FLOAT(15,2) NOT NULL,
+    tipo_prestamo VARCHAR(50) NOT NULL,
+    id_cuenta INT,
+    PRIMARY KEY (id_prestamo),
+    FOREIGN KEY (id_cuenta) REFERENCES Cuenta(id_cuenta)
+);
+
+CREATE TABLE Chequera (
+	id_chequera INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    acabada VARCHAR(20) NOT NULL,
+    id_cuenta INT,
+    PRIMARY KEY (id_chequera),
+    FOREIGN KEY (id_cuenta) REFERENCES Cuenta(id_cuenta)
+);
+
+CREATE TABLE Cheque (
+	id_cheque INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    monto FLOAT(15,2) NOT NULL,
+    autorizado VARCHAR(20) NOT NULL,
+    disponible VARCHAR(20) NOT NULL,
+    id_chequera INT,
+    PRIMARY KEY (id_cheque),
+    FOREIGN KEY (id_chequera) REFERENCES Cuenta(id_chequera)
+);
+
